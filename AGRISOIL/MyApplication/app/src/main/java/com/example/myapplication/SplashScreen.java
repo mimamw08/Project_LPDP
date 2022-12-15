5e8cb470c522f889eb74de91acd06435962aba2c
M Imam Whidyarto <67720586+TwoSels@users.noreply.github.com>
2022-12-15 11:20:37 +0700

    Agrisoil App
    com.agrisoil.nkpapp

---
+++ AGRISOIL/MyApplication/app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() -> {
+            Intent i=new Intent(SplashScreen.this,Welcome_page.class);
+            startActivity(i);
+        },3000);
+    }
+}
